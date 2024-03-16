592ffa731b69df67ce83d7eca3ce0dc5e21a79a5
Ankit <ankitraj1307@outlook.com>
2024-03-16 13:30:51 +0530

    changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,6 +16,7 @@ export const Form = ({ handleColors }) => {
                     onChange={(e) => {
                         setColor(e.target.value);
                     }}
+                    value="#ff0000"
                 ></input>
                 <input
                     type="text"
